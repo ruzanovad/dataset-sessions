ab0f838d433a04d7582a1a3fd5b218a459fb7d54
Daria Ruzanova <kaiser7lu@gmail.com>
2024-08-28 22:59:17 +0600

    feat: it works but still can't get artifacts from folder

--- credit-score/start_services.sh
+++ credit-score/start_services.sh
@@ -4,4 +4,4 @@
 jupyter notebook --notebook-dir=. --ip='0.0.0.0' --port=8888 --no-browser --allow-root --ServerApp.token='' --ServerApp.password=''&
 
 # Start MLflow server
-mlflow server --host 0.0.0.0 --port 5000 --backend-store-uri postgresql://mlflow_user:magical_password@postgres/mlflow_db --default-artifact-root /app/mlflow_artifacts
+mlflow server --host 0.0.0.0 --port 5000 --backend-store-uri postgresql://mlflow_user:magical_password@postgres/mlflow_db --default-artifact-root /app/data/mlflow_artifacts
