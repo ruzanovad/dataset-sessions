3c9658c1bd3f431f129e2816abeb2a59c6fafc7d
Daria Ruzanova <kaiser7lu@gmail.com>
2024-08-28 22:17:39 +0600

    fix: fixed jupyter run command

--- credit-score/start_services.sh
+++ credit-score/start_services.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Start Jupyter Notebook in the background
-jupyter notebook --notebook-dir=. --ip='0.0.0.0' --port=8888 --no-browser --allow-root --NotebookApp.token='' --NotebookApp.password=''&
+jupyter notebook --notebook-dir=. --ip='0.0.0.0' --port=8888 --no-browser --allow-root --ServerApp.token='' --ServerApp.password=''&
 
 # Start MLflow server
 mlflow server --host 0.0.0.0 --port 5000 --backend-store-uri postgresql://mlflow_user:magical_password@postgres/mlflow_db --default-artifact-root /app/mlflow_artifacts
