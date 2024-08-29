6c1729b9feb0dd3952f2b56f0aa654f1a5873bd9
Daria Ruzanova <kaiser7lu@gmail.com>
2024-08-29 20:52:52 +0600

    feat!: a lot happened..

--- credit-score/start_services.sh
+++ credit-score/start_services.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Start Jupyter Notebook in the background
-jupyter notebook --notebook-dir=. --ip='0.0.0.0' --port=8888 --no-browser --allow-root --ServerApp.token='' --ServerApp.password=''&
+jupyter notebook --notebook-dir=. --ip='0.0.0.0' --port=8888 --no-browser --allow-root --IdentityProvider.token='' --ServerApp.password=''&
 
 # Start MLflow server
 mlflow server --host 0.0.0.0 --port 5000 --backend-store-uri postgresql://mlflow_user:magical_password@postgres/mlflow_db --default-artifact-root /app/data/mlflow_artifacts
